0ca620f3cbe36a4b5c9351e1944938c94469c0e8
Vannida Lim <vannida731@gmail.com>
2019-09-11 06:52:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,10 +10,12 @@ end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  return {:name => "Grace Hopper"}
 end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
+  return {:id => 1}
 end
 
 def my_hash_creator(key, value)
